a61469aa5908b7d4d9af92966951877adccdefaf
Tim <Tim@DESKTOP-A5IOUB9>
2020-06-23 23:18:00 +0800

    feat:type page

--- src/main/java/com/timshuns/config/WebConfig.java
+++ src/main/java/com/timshuns/config/WebConfig.java
@@ -1,11 +1,9 @@
 package com.timshuns.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import com.timshuns.interceptor.LoginInterceptor;
 
 @Configuration
